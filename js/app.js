513bb0a9c8d7e480b148fa3791966865752f33d9
Turdiyeva Feruza <feruzaturdiyevashuhrat@gmail.com>
2022-11-18 07:13:57 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+alert("Saytimizga Hush kelibsiz!!!")
+var userName = prompt('UZ : Ismingizni kiriting:' + '\n' + 'En : Enter your name:' + '\n' + 'Tr : Isminizi ekleyin:')
+console.log('Ismi: ' + userName);
+var userlastName = prompt('UZ : Familiyangizni kiriting:' + '\n' + 'En : Enter your last name:' + '\n' + 'Tr : Soyadinizi ekleyin:')
+console.log('Familiyasi: ' + userlastName);
+var userAge = prompt('UZ : Yoshingizni kiriting :' + '\n' + 'En : Enter your age:' + '\n' + 'Tr : Yaşinizi ekleyin:')
+console.log('Yoshi: ' + userAge);
+var userAdress = prompt('UZ : Manzilingizni kiriting :' + '\n' + 'En : Enter your adress:' + '\n' + 'Tr : Ev adresinizi ekleyin:')
+console.log('Manzili: ' + userAdress);
+var userPhoneNumber = prompt('UZ : Telefon raqamingizni kiriting :' + '\n' + 'En : Enter your phone number:' + '\n' + 'Tr : Telefon numaranizi ekleyin:')
+console.log('Telefon raqami: ' + userPhoneNumber);
+alert("Ma'lumotlaringiz qabul qilindi.")
